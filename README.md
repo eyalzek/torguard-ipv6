1a24b4efca5ff5433a2cfdb44c1e38896131e905
eyalzek <eyal.ez@gmail.com>
2015-05-05 12:29:14 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Connect to TorGuard using openvpn and disable IPv6 for the wlan0 interface.
+
+Enable IPv6 once finished.
---
+++ torguardipv6.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+function start {
+    sysctl -w net.ipv6.conf.wlan0.disable_ipv6=1
+    openvpn "/etc/openvpn/TorGuard.$1.conf"
+}
+
+function stop {
+    killall openvpn
+    sysctl -w net.ipv6.conf.wlan0.disable_ipv6=0
+}
+
+function list {
+    folder_path="/etc/openvpn/"
+    for x in $(find "$folder_path" -name "TorGuard*"); do
+        foo=${x/"$folder_path"TorGuard./}
+        echo ${foo/\.conf/}
+    done
+}
+
+function usage {
+    echo "'$0 list'           - will display a list of available servers"
+    echo "'$0 start [SERVER]' - will connect the VPN to the selected server"
+    echo "'$0 stop'           - will stop the VPN"
+    exit 0
+}
+
+if [ $# -lt 1 ]; then
+    usage
+else
+    case $1 in
+        start)
+            if [ $# -lt 2 ]; then
+                usage
+            else
+                start "$2"
+            fi
+            ;;
+
+        stop)
+            stop
+            ;;
+
+        list)
+            list
+            ;;
+
+        *)
+            usage
+            ;;
+    esac
+fi
